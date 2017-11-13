ade21f60043647fa7f0d8b40e1a446047c1a480a
Mikko Teras <mikkoteras@gmail.com>
2017-11-13 16:11:56 +0200

    Add in-place math::clamp().

--- src/math/math_util.h
+++ src/math/math_util.h
@@ -2,13 +2,20 @@
 #define MATH_UTIL_H
 
 namespace math {
-    template<typename t> t clamp(t x, t min, t max) {
+    template<typename t> t clamped(t x, t min, t max) {
         if (x < min)
             return min;
         else if (x > max)
             return max;
         else
             return x;
+    }
+
+    template<typename t> void clamp(t &x, t min, t max) {
+        if (x < min)
+            x = min;
+        else if (x > max)
+            x = max;
     }
 }
 
