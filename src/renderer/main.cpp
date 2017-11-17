b93fb0058fb1c24045274e69a749a876264afc3f
Mikko Teras <mikkoteras@gmail.com>
2017-11-17 10:56:41 +0200

    Fix message typo.

--- src/renderer/main.cpp
+++ src/renderer/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
         return win.run(s);
     }
     catch (...) {
-        std::cerr << "caught stray exceptions, aborting." << std::endl;
+        std::cerr << "caught stray exception, aborting." << std::endl;
         return 1;
     }
 }
