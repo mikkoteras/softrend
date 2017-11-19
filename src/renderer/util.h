4370f652a497753c03a1389ac81aad658bdc1276
Mikko Teras <mikkoteras@gmail.com>
2017-11-19 20:30:04 +0200

    Remove redundant (and incorrect) functions.

--- src/renderer/util.h
+++ src/renderer/util.h
@@ -22,26 +22,6 @@ namespace util {
         result << "]";
         return result.str();
     }
-
-    template<typename float_t, int dim>
-    vector<float_t, dim> min (const math::vector<float_t, dim> &v1, const vector<float_t, dim> &v2) {
-        vector<float_t, dim> result;
-
-        for (int i = 0; i < dim; ++i)
-            result[i] = v1[0] < v2[0] ? v1[i] : v2[i];
-
-        return result;
-    }
-
-    template<typename float_t, int dim>
-    vector<float_t, dim> max (const vector<float_t, dim> &v1, const vector<float_t, dim> &v2) {
-        vector<float_t, dim> result;
-
-        for (int i = 0; i < dim; ++i)
-            result[i] = v1[0] > v2[0] ? v1[i] : v2[i];
-
-        return result;
-    }
 }
 
 #endif
