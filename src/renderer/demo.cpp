7789ff8bc036c780301d5a7cb86e642f3615c616
Mikko Teras <mikkoteras@gmail.com>
2017-11-24 08:14:15 +0200

    Cleaner implementation of fern and a better look.

--- src/renderer/demo.cpp
+++ src/renderer/demo.cpp
@@ -1,5 +1,6 @@
 #include "demo.h"
 #include "framebuffer.h"
+#include "math_detail.h"
 #include "vector.h"
 #include "SDL.h"
 #include <cmath>
@@ -7,7 +8,9 @@
 using namespace math;
 using namespace std;
 
-demo::demo() {
+demo::demo() :
+    scene() {
+    
     create_fern();
 }
 
@@ -28,9 +31,9 @@ void demo::render(framebuffer &fb) {
 void demo::render_fern_still(framebuffer &fb) {
     float t = 0.0f;
     
-    set_eye_position(vector3f{10 * cosf(t), 2.0f, 10 * sinf(t)});
+    set_eye_position(vector3f{10 * cosf(t), 4.0f, 10 * sinf(t)});
     set_eye_reference_point(vector3f{0.0f, 1.0f, 0.0f});
-    set_eye_orientation(vector3f{0.0f, 1.0f, 0.0f});
+    set_eye_orientation(vector3f{0.0f, 2.0f, 0.0f});
     
     set_view_to_view_plane_distance(2);
     
@@ -40,8 +43,8 @@ void demo::render_fern_still(framebuffer &fb) {
 void demo::render_fern_3d(framebuffer &fb) {
     float t = clock.seconds();
     
-    set_eye_position(vector3f{10 * cosf(t), 2.0f, 10 * sinf(t)});
-    set_eye_reference_point(vector3f{0.0f, 1.0f, 0.0f});
+    set_eye_position(vector3f{10 * cosf(t), 85.0f, 10 * sinf(t)});
+    set_eye_reference_point(vector3f{0.0f, 0.0f, 0.0f});
     set_eye_orientation(vector3f{0.0f, 1.0f, 0.0f});
     
     set_view_to_view_plane_distance(2);
@@ -67,36 +70,35 @@ void demo::key_down_event(int sdl_keycode, bool ctrl_is_down) {
 void demo::create_fern() {
     create_fern_recursive(vector3f{0.0f, 0.0f, 0.0f},
                           color(0.0f, 1.0f, 0.5f, 1.0f),
-                          3.0f,
+                          5.0f,
                           0.0f,
-                          3.14159265f / 2.0f,
-                          4);
+                          detail::pi<float>() / 2.0f,
+                          3);
 }
 
 void demo::create_fern_recursive(const vector3f &root,
                                  const color &root_color,
                                  float len, float phi, float theta, int generations) {
-    vector3f top_direction = len * vector3f{
-        cosf(phi) * cosf(theta),
-        sinf(theta),
-        -sinf(phi) * cosf(theta)
-    };
+    vector3f top_direction = len * vector3f{cosf(phi) * cosf(theta), sinf(theta), -sinf(phi) * cosf(theta)};
     color top_color(0.0f, 0.2f, 0.1f, 1.0f);
-    
+
     fern.add_line(root, root + top_direction, root_color, top_color);
 
-    const int branches = 7;
-    float branch_position[branches]               = {  0.2f,  0.4f,   0.55f, 0.65f,  0.75f,  0.90f,  1.0f  };
-    float branch_phi_direction_offset[branches]   = {  1.1f,  2.85f,  4.5f,  1.8f,   5.75f, -2.65f, -0.6f  };
-    float branch_theta_direction_offset[branches] = { -0.49f, 0.47f, -0.46f, 0.44f, -0.33f,  0.39f,  0.22f };
-    float branch_length[branches]                 = {  0.6f,  0.5f,   0.4f,  0.3f,   0.2f,   0.1f,   0.1f  };
+    if (generations > 0) {
+        const int phi_branches = 5;
+        const int theta_branches = 7;
 
-    if (generations > 0)
-        for (int i = 0; i < branches; ++i)
-            create_fern_recursive(root + branch_position[i] * top_direction,
-                                  color(root_color, top_color, branch_position[i]),
-                                  branch_length[i] * len,
-                                  branch_phi_direction_offset[i] + phi,
-                                  branch_theta_direction_offset[i] + theta,
-                                  generations - 1);
+        for (int j = 0; j < theta_branches; ++j) {
+            float branch_pos = 1.0f - powf(2.0f / 3.0f, j + 1);
+
+            for (int i = 0; i < phi_branches; ++i) {
+                create_fern_recursive(root + branch_pos * top_direction,
+                                      color(root_color, top_color, branch_pos),
+                                      len / (3 * j + 2),
+                                      2.0f * detail::pi<float>() * i / phi_branches,
+                                      theta + 0.7f,
+                                      generations - 1);
+            }
+        }
+    }
 }
