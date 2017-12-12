f1b3bdc7e9f8a71ef185ca34f0446ad1e35af97e
Mikko Teras <mikkoteras@gmail.com>
2017-12-12 20:37:20 +0200

    Add benchmark_frame.

---
+++ src/renderer/benchmark_frame.cpp
@@ -0,0 +1,103 @@
+#include "benchmark_frame.h"
+
+using namespace std;
+using namespace std::chrono;
+
+benchmark_frame::benchmark_frame() :
+    frame_number(-1) {
+}
+
+benchmark_frame::benchmark_frame(frame_number_t frame_number, timestamp_t frame_started) :
+    frame_number(frame_number),
+    frame_started(frame_started) {
+}
+
+benchmark_frame::~benchmark_frame() {
+}
+
+benchmark_frame::frame_number_t benchmark_frame::get_frame_number() const {
+    return frame_number;
+}
+
+benchmark_frame::timestamp_t benchmark_frame::clear_starting() {
+    return high_resolution_clock::now();
+}
+
+void benchmark_frame::clear_finished(timestamp_t start_timestamp) {
+    clear_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
+}
+
+benchmark_frame::timestamp_t benchmark_frame::compute_starting() {
+    return high_resolution_clock::now();
+}
+
+void benchmark_frame::compute_finished(timestamp_t start_timestamp) {
+    compute_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
+}
+
+benchmark_frame::timestamp_t benchmark_frame::render_starting() {
+    return high_resolution_clock::now();
+}
+
+void benchmark_frame::render_finished(timestamp_t start_timestamp) {
+    render_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
+}
+
+benchmark_frame::timestamp_t benchmark_frame::copy_starting() {
+    return high_resolution_clock::now();
+}
+
+void benchmark_frame::copy_finished(timestamp_t start_timestamp) {
+    copy_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
+}
+
+void benchmark_frame::finish() {
+    frame_finished = high_resolution_clock::now();
+}
+
+double benchmark_frame::time_spent_clearing() const {
+    return clear_time;
+}
+
+double benchmark_frame::time_spent_computing() const {
+    return compute_time;
+}
+
+double benchmark_frame::time_spent_rendering() const {
+    return render_time;
+}
+
+double benchmark_frame::time_spent_copying() const {
+    return copy_time;
+}
+
+double benchmark_frame::average_frame_time(const benchmark_frame &rhs) const {
+    if (frame_number > rhs.frame_number - 1)
+        return 0.0;
+
+    return duration<double, milli>(rhs.frame_finished - frame_finished).count() / (rhs.frame_number - frame_number);
+        
+}
+
+double benchmark_frame::average_frames_per_second(const benchmark_frame &rhs) const {
+    if (frame_number > rhs.frame_number - 1)
+        return 0.0;
+
+    return (rhs.frame_number - frame_number) / duration<double>(rhs.frame_finished - frame_finished).count();
+}
+
+const benchmark_frame &benchmark_frame::operator+=(const benchmark_frame &rhs) {
+    clear_time += rhs.clear_time;
+    compute_time += rhs.compute_time;
+    render_time += rhs.render_time;
+    copy_time += rhs.copy_time;
+    return *this;
+}
+
+const benchmark_frame &benchmark_frame::operator-=(const benchmark_frame &rhs) {
+    clear_time -= rhs.clear_time;
+    compute_time -= rhs.compute_time;
+    render_time -= rhs.render_time;
+    copy_time -= rhs.copy_time;
+    return *this;
+}
---
+++ src/renderer/benchmark_frame.h
@@ -0,0 +1,48 @@
+#ifndef BENCHMARK_FRAME_H
+#define BENCHMARK_FRAME_H
+
+#include <chrono>
+
+class benchmark_frame {
+public:
+    typedef std::chrono::high_resolution_clock::time_point timestamp_t;
+    typedef unsigned frame_number_t;
+
+    benchmark_frame();
+    benchmark_frame(frame_number_t frame_number, timestamp_t frame_started);
+    ~benchmark_frame();
+
+    frame_number_t get_frame_number() const;
+    
+    timestamp_t clear_starting();
+    void clear_finished(timestamp_t start_timestamp);
+    timestamp_t compute_starting();
+    void compute_finished(timestamp_t start_timestamp);
+    timestamp_t render_starting();
+    void render_finished(timestamp_t start_timestamp);
+    timestamp_t copy_starting();
+    void copy_finished(timestamp_t start_timestamp);
+    void finish();
+
+    double time_spent_clearing() const;
+    double time_spent_computing() const;
+    double time_spent_rendering() const;
+    double time_spent_copying() const;
+
+    double average_frame_time(const benchmark_frame &rhs) const;
+    double average_frames_per_second(const benchmark_frame &rhs) const;
+    
+    const benchmark_frame &operator+=(const benchmark_frame &rhs);
+    const benchmark_frame &operator-=(const benchmark_frame &rhs);
+    
+private:
+    frame_number_t frame_number;
+    timestamp_t frame_started;
+    timestamp_t frame_finished;
+    double clear_time = 0.0;
+    double compute_time = 0.0;
+    double render_time = 0.0;
+    double copy_time = 0.0;
+};
+
+#endif
