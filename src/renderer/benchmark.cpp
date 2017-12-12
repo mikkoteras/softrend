751fb0885b79617211089eed262f1f390385e5d3
Mikko Teras <mikkoteras@gmail.com>
2017-12-12 20:36:46 +0200

    Make benchmark thread-safe.

--- src/renderer/benchmark.cpp
+++ src/renderer/benchmark.cpp
@@ -5,107 +5,47 @@
 using namespace std;
 using namespace std::chrono;
 
-typedef benchmark::timestamp_t timestamp_t;
-
 benchmark::benchmark() {
 }
 
 benchmark::~benchmark() {
 }
 
-timestamp_t benchmark::frame_starting() {
-    auto entered = high_resolution_clock::now();
-
-    frame_in_progress = frame();
-    frame_in_progress.frame_number = frames_created;
-    frame_in_progress.frame_started = entered;
-    frames_created += 1;
-
-    return entered;
+benchmark_frame benchmark::frame_starting() {
+    return benchmark_frame(frames_created++, high_resolution_clock::now());
 }
 
-void benchmark::frame_finished(timestamp_t start_timestamp) {
-    auto entered = high_resolution_clock::now();
-    frame_in_progress.frame_finished = entered;
-
-    if (frame_history.size() >= max_history_length) {
+void benchmark::frame_finished(benchmark_frame &frame) {
+    frame.finish();
+    history_sum += frame;
+    frame_history.push_back(frame);
+    
+    while (frame_history.size() > max_history_length) {
         history_sum -= frame_history.front();
         frame_history.pop_front();
     }
-
-    history_sum += frame_in_progress;
-    frame_history.push_back(frame_in_progress);
-}
-
-timestamp_t benchmark::clear_starting() {
-    return high_resolution_clock::now();
-}
-
-void benchmark::clear_finished(timestamp_t start_timestamp) {
-    frame_in_progress.clear_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
-}
-
-timestamp_t benchmark::compute_starting() {
-    return high_resolution_clock::now();
-}
-
-void benchmark::compute_finished(timestamp_t start_timestamp) {
-    frame_in_progress.compute_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
-}
-
-timestamp_t benchmark::render_starting() {
-    return high_resolution_clock::now();
-}
-
-void benchmark::render_finished(timestamp_t start_timestamp) {
-    frame_in_progress.render_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
-}
-
-timestamp_t benchmark::copy_starting() {
-    return high_resolution_clock::now();
-}
-
-void benchmark::copy_finished(timestamp_t start_timestamp) {
-    frame_in_progress.copy_time += duration<double, milli>(high_resolution_clock::now() - start_timestamp).count();
 }
 
 string benchmark::get_stats() const {
-    if (frame_history.empty())
-        return "no benchmark data";
-
     int frames = frame_history.size();
-    double total_time = duration<double, milli>(frame_history.back().frame_finished -
-                                                frame_history.front().frame_started).count();
-    double frame_time = total_time / frames;
-    double fps = 1000 * frames / total_time;
-    double clearing = history_sum.clear_time / frame_history.size();
-    double computing = history_sum.compute_time / frame_history.size();
-    double rendering = history_sum.render_time / frame_history.size();
-    double copying = history_sum.copy_time / frame_history.size();
+
+    if (frames < 2)
+        return "no benchmark data";
+    
+    double avg_frame_time = frame_history.front().average_frame_time(frame_history.back());
+    double fps = frame_history.front().average_frames_per_second(frame_history.back());
+    double clearing = history_sum.time_spent_clearing() / frames;
+    double computing = history_sum.time_spent_computing() / frames;
+    double rendering = history_sum.time_spent_rendering() / frames;
+    double copying = history_sum.time_spent_copying() / frames;
 
     ostringstream result;
     result << fixed << setw(6) << setprecision(2)
-           << "frame time: " << frame_time
+           << "frame time: " << avg_frame_time
            << " ms | fps: " << fps
            << " | clear: " << clearing
            << " ms | compute: " << computing
            << " ms | render: " << rendering
            << " ms | copying: " << copying << " ms";
     return result.str();
-}
-
-const benchmark::frame &benchmark::frame::operator+=(const frame &rhs) {
-    clear_time += rhs.clear_time;
-    compute_time += rhs.compute_time;
-    render_time += rhs.render_time;
-    copy_time += rhs.copy_time;
-    return *this;
-}
-
-const benchmark::frame &benchmark::frame::operator-=(const frame &rhs) {
-    clear_time -= rhs.clear_time;
-    compute_time -= rhs.compute_time;
-    render_time -= rhs.render_time;
-    copy_time -= rhs.copy_time;
-    return *this;
 }
--- src/renderer/window.h
+++ src/renderer/window.h
@@ -3,6 +3,8 @@
 
 #include "SDL_ttf.h"
 
+class benchmark_frame;
+class framebuffer;
 class scene;
 class SDL_Window;
 class SDL_Renderer;
@@ -18,17 +20,27 @@ public:
     int run(scene &s);
 
 private:
+    void clear_framebuffer(scene &s, framebuffer &fb, benchmark_frame &frame_stats);
+    void update_scene(scene &s, framebuffer &fb, benchmark_frame &frame_stats);
+    void prepare_sdl_texture(scene &s, const framebuffer &fb, benchmark_frame &frame_stats);
+    bool read_user_input(scene &s);
+
+private:
     bool init_sdl();
     void deinit_sdl();
     void render_text_overlay(scene &s);
 
 private:
     int width, height;
+    static const int concurrent_stages = 3;
+    framebuffer *framebuffers[concurrent_stages];
+
+private:
     bool sdl_context_initialized = false;
     SDL_Window *sdl_window = nullptr;
     SDL_Renderer *sdl_renderer = nullptr;
-    SDL_Texture *sdl_texture = nullptr;  // TODO maybe multiple
-
+    SDL_Texture *sdl_texture = nullptr; // TODO maybe multiple
+    
 private:
     bool text_overlay_visible = false;
     TTF_Font *text_overlay_font = nullptr;
