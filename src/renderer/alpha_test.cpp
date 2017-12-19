65a8e6a51100ce5e997e849e7ef152478b3db0fb
Mikko Teras <mikkoteras@gmail.com>
2017-12-19 14:07:11 +0200

    Add backdrop object.

--- src/renderer/alpha_test.cpp
+++ src/renderer/alpha_test.cpp
@@ -11,6 +11,7 @@ alpha_test::alpha_test(bool echo_comments) :
     scene() {
 
     try {
+        backdrop = importer::load_wavefront_object("backdrop.obj", materials(), echo_comments);
         blue_icosa = importer::load_wavefront_object("blue_icosa.obj", materials(), echo_comments);
     }
     catch (importer::importer_exception) {
@@ -18,16 +19,18 @@ alpha_test::alpha_test(bool echo_comments) :
         return;
     }
 
-    light_sources().set_ambient_light(color(0.5f, 0.5f, 0.5f, 0.5f));
-    light_sources().add_light(directional_light(vector3f{0.0f, -1.0f, 0.0f}, color(0.5f, 0.5f, 0.5f, 0.5f)));
+    light_sources().set_ambient_light(color(0.2f, 0.2f, 0.2f, 1.0f));
+    light_sources().add_light(directional_light(vector3f{0.0f, -1.0f, 0.0f}, color(0.5f, 0.5f, 0.5f, 1.0f)));
+
+    set_eye_position(vector3f{0.0f, 0.0f, 10.0f});
+    set_eye_reference_point(vector3f{0.0f, 0.0f, 0.0f});
+    set_eye_orientation(vector3f{0.0f, 1.0f, 0.0f});
+    set_fov(120.0f / (2.0f * math::detail::pi<float>()));
+
+    backdrop.set_position(0, 0, -10);
     
     blue_icosa.set_position(0, 0, 0);
 
-    set_eye_position(vector3f{0.0f, 0.0f, 5.0f});
-    set_eye_reference_point(vector3f{0.0f, 0.0f, 0.0f});
-    set_eye_orientation(vector3f{0.0f, 1.0f, 0.0f});
-
-    set_fov(120.0f / (2.0f * math::detail::pi<float>()));
 }
 
 alpha_test::~alpha_test() {
@@ -37,6 +40,8 @@ void alpha_test::render(framebuffer &fb) {
     float t = clock.seconds();
 
     blue_icosa.set_rotation(0, t, t / 10.0f);
-    
+
+    backdrop.set_scaling(.5f, .5f, .5f);
+    backdrop.render(*this, fb, false, false);
     blue_icosa.render(*this, fb, visualize_normals, visualize_reflection_vectors);
 }
