826d8ae66ffc955d7928c2f923355a8b3730071a
Mikko Teras <mikkoteras@gmail.com>
2017-12-19 12:34:02 +0200

    Add math::detail::tan().

--- src/math/math_detail.h
+++ src/math/math_detail.h
@@ -5,6 +5,7 @@ namespace math {
     namespace detail {
         template<typename float_t> float_t sin(float_t t);
         template<typename float_t> float_t cos(float_t t);
+        template<typename float_t> float_t tan(float_t t);
         template<typename float_t> float_t asin(float_t t);
         template<typename float_t> float_t acos(float_t t);
         template<typename float_t> float_t floor(float_t t);
