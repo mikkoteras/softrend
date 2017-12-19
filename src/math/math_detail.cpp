264cbe0e2a98b57cf4ec04c5aaadb01b77227b46
Mikko Teras <mikkoteras@gmail.com>
2017-12-19 12:34:02 +0200

    Add math::detail::tan().

--- src/math/math_detail.cpp
+++ src/math/math_detail.cpp
@@ -3,19 +3,21 @@
 
 namespace math {
     namespace detail {
-        template<> float sin<float>(float t) { return ::sinf(t); }
-        template<> float cos<float>(float t) { return ::cosf(t); }
-        template<> float asin<float>(float t) { return ::asinf(t); }
-        template<> float acos<float>(float t) { return ::acosf(t); }
-        template<> float floor<float>(float t) { return ::floorf(t); }
+        template<> float sin<float>(float t)      { return ::sinf(t);   }
+        template<> float cos<float>(float t)      { return ::cosf(t);   }
+        template<> float tan<float>(float t)      { return ::tanf(t);   }
+        template<> float asin<float>(float t)     { return ::asinf(t);  }
+        template<> float acos<float>(float t)     { return ::acosf(t);  }
+        template<> float floor<float>(float t)    { return ::floorf(t); }
 
-        template<> double sin<double>(double t) { return ::sin(t); }
-        template<> double cos<double>(double t) { return ::cos(t); }
-        template<> double asin<double>(double t) { return ::asin(t); }
-        template<> double acos<double>(double t) { return ::acos(t); }
-        template<> double floor<double>(double t) { return ::floor(t); }
+        template<> double sin<double>(double t)   { return ::sin(t);    }
+        template<> double cos<double>(double t)   { return ::cos(t);    }
+        template<> double tan<double>(double t)   { return ::tan(t);    }
+        template<> double asin<double>(double t)  { return ::asin(t);   }
+        template<> double acos<double>(double t)  { return ::acos(t);   }
+        template<> double floor<double>(double t) { return ::floor(t);  }
 
-        template<> float pi<float>() { return 3.14159265358979323846f; }
-        template<> double pi<double>() { return 3.14159265358979323846; }
+        template<> float pi<float>()              { return 3.14159265358979323846f; }
+        template<> double pi<double>()            { return 3.14159265358979323846;  }
     }
 }
