6c5b24ee422dc7c725092f6d5cf44c101deca31c
Mikko Teras <mikkoteras@gmail.com>
2017-12-20 13:29:48 +0200

    Add a simple benchmark for vector interpolation.

--- src/math_tests/main.cpp
+++ src/math_tests/main.cpp
@@ -3,25 +3,24 @@
 #include "test_harness.h"
 #include "util_tests.h"
 #include "vector_tests.h"
+#include "vector_benchmarks.h"
 #include <iostream>
 
-void run_all_tests();
+void run_everything();
 
 int main()
 {
-    run_all_tests();
+    run_everything();
 
-    //char dummy;
-    //std::cout << "Ready to exit..." << std::endl;
-    //std::cin >> dummy;
     return 0;
 }
 
-void run_all_tests() {
-    test_harness::the_tester = new test_harness;
+void run_everything() {
+    test_harness harness;
+    
     vector_tests();
     matrix_tests();
     linear_transform_tests();
     util_tests();
-    delete test_harness::the_tester;
+    vector_benchmarks();
 }
--- src/math_tests/test_harness.h
+++ src/math_tests/test_harness.h
@@ -4,6 +4,7 @@
 #define ASSERT(x) test_harness::the_tester->test((x), __FILE__, __LINE__)
 
 #include <string>
+#include <chrono>
 
 class test_harness {
 public:
@@ -11,10 +12,15 @@ public:
     ~test_harness();
 
     void test(bool cond, const std::string &filename, int line_number);
+
+    void start_clock();
+    void stop_clock(const std::string &benchmark);
+    
     static test_harness *the_tester;
 
 private:
     int tests_tried, tests_passed, tests_failed;
+    std::chrono::high_resolution_clock::time_point benchmark_start_time;
 };
 
 #endif
