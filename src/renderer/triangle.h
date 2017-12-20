dbcdcf7a121b8e45a6861cccbe0ef1da8058ba4b
Mikko Teras <mikkoteras@gmail.com>
2017-12-20 15:56:45 +0200

    Remove render_dumb().

--- src/renderer/triangle.h
+++ src/renderer/triangle.h
@@ -63,14 +63,6 @@ public:
     void visualize_reflection_vectors(framebuffer &target, const mesh &parent_mesh,
                                       scene &parent_scene, const math::matrix4x4f &local_to_view) const;
 
-    //private: // TODO legacy, remove
-    void render_dumb(framebuffer &target, const mesh &parent_mesh, const scene &parent_scene) const;
-    struct edge { int top, bottom; }; // indices to vertex_index
-    edge create_edge(int vi1, int vi2, const math::vector3f *vertex_data) const;
-    int find_long_edge(edge *edges, const math::vector3f *vertex_data) const;
-    void draw_half_triangle(const edge &long_edge, const edge &short_edge, framebuffer &target,
-                            const mesh &parent_mesh, const scene &parent_scene) const;
-
 private:
     int vertex_index[3]; // indices to parent mesh's vertex data
     math::vector3f vertex_uv[3];
