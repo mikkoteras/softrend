d4586f14ca27b4d6cb96f4b3add35845b25896a8
Mikko Teras <mikkoteras@gmail.com>
2018-01-15 13:05:53 +0200

    Add an alternative implementation for thread_pool.

--- src/renderer/thread_pool.cpp.h
+++ src/renderer/thread_pool.cpp.h
@@ -1,53 +1,80 @@
 #include "thread_pool.h"
 
-template<typename parent_class> thread_pool<parent_class>::thread_pool(parent_class *parent, unsigned num_workers) :
-    parent(parent),
-    thread_busy(num_workers, false) {
+#if THREAD_POOL_REUSE_THREADS
 
-    for (unsigned i = 0; i < num_workers; ++i)
-        threads.push_back(std::thread(&thread_pool::loop, this, i));
-}
+    template<typename parent_class>
+    thread_pool<parent_class>::thread_pool(parent_class *parent, unsigned num_threads) :
+        parent(parent),
+        thread_busy(num_threads, false) {
 
-template<typename parent_class> thread_pool<parent_class>::~thread_pool() {
-    std::unique_lock<std::mutex> lock(work_mutex);
-    stopping = true;
-    threads_activated.notify_all();
-    lock.unlock();
-
-    while (!threads.empty()) {
-        threads.back().join();
-        threads.pop_back();
+        for (unsigned i = 0; i < num_threads; ++i)
+            threads.push_back(std::thread(&thread_pool::loop, this, i));
     }
-}
 
-template<typename parent_class> void thread_pool<parent_class>::execute(void(parent_class::*func)(unsigned thread_index)) {
-    std::unique_lock<std::mutex> lock(work_mutex);
-    work_function = func;
-    num_threads_busy = static_cast<unsigned>(threads.size());
+    template<typename parent_class> thread_pool<parent_class>::~thread_pool() {
+        std::unique_lock<std::mutex> lock(work_mutex);
+        stopping = true;
+        threads_activated.notify_all();
+        lock.unlock();
 
-    for (unsigned i = 0; i < thread_busy.size(); ++i)
-        thread_busy[i] = true;
-
-    threads_activated.notify_all();
-    threads_completed.wait(lock);
-    work_function = nullptr;
-}
-
-template<typename parent_class> void thread_pool<parent_class>::loop(unsigned thread_index) {
-    std::unique_lock<std::mutex> lock(work_mutex);
-
-    while (!stopping) {
-        if (!thread_busy[thread_index])
-            threads_activated.wait(lock);
-
-        if (thread_busy[thread_index] && work_function) {
-            lock.unlock();
-            (parent->*work_function)(thread_index);
-            lock.lock();
-            thread_busy[thread_index] = false;
-
-            if (!(--num_threads_busy))
-                threads_completed.notify_one();
+        while (!threads.empty()) {
+            threads.back().join();
+            threads.pop_back();
         }
     }
-}
+
+    template<typename parent_class>
+    void thread_pool<parent_class>::execute(void(parent_class::*func)(unsigned thread_index)) {
+        std::unique_lock<std::mutex> lock(work_mutex);
+        work_function = func;
+        num_threads_busy = static_cast<unsigned>(threads.size());
+        
+        for (unsigned i = 0; i < thread_busy.size(); ++i)
+            thread_busy[i] = true;
+
+        threads_activated.notify_all();
+        threads_completed.wait(lock);
+        work_function = nullptr;
+    }
+
+    template<typename parent_class> void thread_pool<parent_class>::loop(unsigned thread_index) {
+        std::unique_lock<std::mutex> lock(work_mutex);
+
+        while (!stopping) {
+            if (!thread_busy[thread_index])
+                threads_activated.wait(lock);
+
+            if (thread_busy[thread_index] && work_function) {
+                lock.unlock();
+                (parent->*work_function)(thread_index);
+                lock.lock();
+                thread_busy[thread_index] = false;
+
+                if (!(--num_threads_busy))
+                    threads_completed.notify_one();
+            }
+        }
+    }
+
+#else // !THREAD_POOL_REUSE_THREADS
+
+    template<typename parent_class>
+    thread_pool<parent_class>::thread_pool(parent_class *parent, unsigned num_threads) :
+        parent(parent),
+        threads(num_threads),
+        num_threads(num_threads) {
+    }
+
+    template<typename parent_class> thread_pool<parent_class>::~thread_pool() {
+    }
+
+    template<typename parent_class>
+    void thread_pool<parent_class>::execute(void(parent_class::*func)(unsigned thread_index)) {
+        for (unsigned i = 0; i < num_threads; ++i)
+            threads.emplace(threads.cbegin() + i, std::move(std::thread(func, parent, i)));
+
+        for (unsigned i = 0; i < num_threads; ++i)
+            threads[i].join();
+    }
+
+#endif
--- src/renderer/thread_pool.h
+++ src/renderer/thread_pool.h
@@ -1,6 +1,7 @@
 #ifndef THREAD_POOL
 #define THREAD_POOL
 
+#include "config/optimizations.h"
 #include <condition_variable>
 #include <mutex>
 #include <thread>
@@ -9,17 +10,18 @@
 template<typename parent_class> class thread_pool {
 public:
     thread_pool() = delete;
-    thread_pool(parent_class *parent, unsigned num_workers);
+    thread_pool(parent_class *parent, unsigned num_threads);
     ~thread_pool();
 
     void execute(void(parent_class::*func)(unsigned thread_index));
 
 private:
-    void loop(unsigned thread_index);
-
     parent_class *parent;
     std::vector<std::thread> threads;
 
+#if THREAD_POOL_REUSE_THREADS
+    void loop(unsigned thread_index);
+    
     std::mutex work_mutex;
     std::condition_variable threads_activated;
     std::condition_variable threads_completed;
@@ -27,6 +29,9 @@ private:
     unsigned num_threads_busy = 0;
     void(parent_class::*work_function)(unsigned thread_index) = nullptr;
     bool stopping = false;
+#else
+    unsigned num_threads;
+#endif
 };
 
 #include "thread_pool.cpp.h"
